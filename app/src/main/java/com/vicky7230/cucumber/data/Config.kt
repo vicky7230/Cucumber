7b1ef50346cfea3e8386da60d81514465d618947
Vipin Kumar <vicky7230@gmail.com>
2018-09-18 18:36:06 +0530

    Fixed HTTP 403 issue

--- app/src/main/java/com/vicky7230/cucumber/data/Config.kt
+++ app/src/main/java/com/vicky7230/cucumber/data/Config.kt
@@ -4,7 +4,7 @@ package com.vicky7230.cucumber.data
  * Created by vicky on 31/12/17.
  */
 object Config {
-    val BASE_URL = "http://food2fork.com/api/"
-    val API_KEY = "abfccec19f3864358e4607a2b284b0c9"//vicky7230@gmail.com
+    val BASE_URL = "https://www.food2fork.com/api/"
+    val API_KEY = "900c1552f029ce0b34f507e6dcb8385a"//vicky7230@gmail.com
     val DB_NAME = "recipes_db"
 }
